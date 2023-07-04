0df597b4d6c5abc20e9fa1adfca8104845b4cf4c
MiriamSanchez11 <79716300+MiriamSanchez11@users.noreply.github.com>
2023-07-04 13:32:23 +0200

    Add files via upload

---
+++ Dato/Usuario.cs
@@ -0,0 +1,32 @@
+﻿using BrainLinkConnect.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainLinkConnect.Dato
+{
+    public class Usuario
+    {
+        List<UsuarioModel> lista = new List<UsuarioModel>();
+        /// <summary>
+        /// Guarda nombre y edad del usuario
+        ///
+        /// </summary>
+        /// <param name="modelo">nombre y edad del usuario</param>
+        public void Guardar(UsuarioModel modelo)
+        {
+            lista.Add(modelo);
+        } 
+        /// <summary>
+        /// Consulta todos los usuarios
+        /// </summary>
+        /// <returns>datos de usuarios</returns>
+        public List<UsuarioModel> Consultar()
+        {
+            return lista;
+
+        }
+    }
+}
---
+++ Modelo/UsuarioModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainLinkConnect.Modelo
+{
+    public class UsuarioModel
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; } 
+    }
+}
